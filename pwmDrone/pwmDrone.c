72cb3cd425a067e6a111f452ca8c1641081030b5
Rudis <rudisarg@gmail.com>
2025-04-03 20:29:51 -0600

    added hardware pwm lib to user requested libraries

--- pwmDrone/pwmDrone.c
+++ pwmDrone/pwmDrone.c
@@ -2,23 +2,23 @@
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
 
-
-
-
 int main()
 {
     stdio_init_all();
 
-    // Initialise the Wi-Fi chip
-    if (cyw43_arch_init()) {
-        printf("Wi-Fi init failed\n");
+    // initializing the Wi-Fi chip
+    if (cyw43_arch_init())
+    {
+        printf("Wi-Fi init failed\n"); // print statement if initializing the Wi-Fi chip fails
         return -1;
     }
 
-    // Example to turn on the Pico W LED
-    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+    // onboard LED is connected to the Wi-Fi/Bluetooth chip so we must access this chip to enable the onboard LED
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1); // setting the output of the LED to 1 which turns ON the LED
+                                                   // CYW43_WL_GPIO_LED_PIN from pic2_w.h file
 
-    while (true) {
+    while (true)
+    {
         printf("Hello, world!\n");
         sleep_ms(1000);
     }
