29931df5d67b0ff05f12efeae5d5ab0b6057a746
Rudis <rudisarg@gmail.com>
2025-04-11 12:36:34 -0600

    added pin definitions for the pins the are going to be used for PWM

--- pwmDrone/pwmDrone.c
+++ pwmDrone/pwmDrone.c
@@ -2,6 +2,14 @@
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
 
+// pico has 24 pwm channels -> datasheet pg. 4
+// using pins 0 - 3 to control the mosfets
+#define PWM_PIN0 0     // this corresponds to motor 1
+#define PWM_PIN2 1     // this corresponds to motor 2
+#define PWM_PIN3 2     // this corresponds to motor 3
+#define PWM_PIN4 3     // this corresponds to motor 4
+#define PWM_FREQ 20000 // 20kHz freq for motor control, I believe prof said this is a good one to use anything beyond this is not nesccary
+
 int main()
 {
     stdio_init_all();
@@ -17,10 +25,13 @@ int main()
     cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0); // setting the output of the LED to off
                                                    // CYW43_WL_GPIO_LED_PIN from pic2_w.h file
 
+    // set up for all four pwm, four pins will be used for pins 0-3
+    // each pin controls a mosfet that and that allows the motor to spin based on our duty cycle
+
     while (true)
     {
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1); // turning LED on
-        sleep_ms(1000);
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1); // turning LED on, start of going through drone cycle
+
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0); // turning LED off
         sleep_ms(1000);
     }
